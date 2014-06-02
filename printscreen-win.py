563fb099eeb34eabfe559b0a77e98a89663ef547
Ivan Dachev <idachev@vmware.com>
2014-06-02 15:34:35 +0300

    Updated

--- printscreen-win.py
+++ printscreen-win.py
@@ -6,21 +6,23 @@ from PIL import ImageGrab
 MSG_TITLE = 'Print Screen'
 
 try:
-   userHome = os.path.expanduser('~')
-   desktopPath = userHome + '/Desktop/'
+  userHome = os.path.expanduser('~')
+  desktopPath = userHome + '/Desktop/'
+  srcPath = os.path.join(desktopPath, 'screen')
+  if not os.path.exists(srcPath):
+    os.makedirs(srcPath)
 
-   im = ImageGrab.grab()
-   dt = datetime.today().strftime("%y%m%d-%H%M%S")
-   imgPath = 'screen-%s.png' % dt
-   srcPath = os.path.join(desktopPath, 'screen')
-   if not os.path.exists(srcPath):
-     os.makedirs(srcPath)
-   imgPath = os.path.join(srcPath, imgPath)
-   im.save(imgPath,'PNG')
-   msg = 'Saved to: %s' % imgPath
-   print(msg)
-   ctypes.windll.user32.MessageBoxA(0, msg, MSG_TITLE, 0)
+  dt = datetime.today().strftime("%y%m%d-%H%M%S")
+  imgPath = 'screen-%s.png' % dt
+  imgPath = os.path.join(srcPath, imgPath)
+
+  im = ImageGrab.grab()
+  im.save(imgPath,'PNG')
+
+  msg = 'Saved: %s' % imgPath
+  print(msg)
+  ctypes.windll.user32.MessageBoxA(0, msg, MSG_TITLE, 0)
 except Exception as e:
-   msg = 'Error: %s' % str(e)
-   print(msg)
-   ctypes.windll.user32.MessageBoxA(0, msg, MSG_TITLE, 0)
+  msg = 'Error: %s' % str(e)
+  print(msg)
+  ctypes.windll.user32.MessageBoxA(0, msg, MSG_TITLE, 0)
